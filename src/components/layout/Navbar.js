94f16680d2266e6480b45d287913dc09d01ebac3
bhanu yadav <yadavbhanu42@gmail.com>
2021-05-28 00:33:21 +0530

    fist commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            AssessMenttask
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" exact to="/">
+                  Home
+                </Link>
+              </li>
+              
+            </ul>
+            <form class="d-flex">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button class="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+        <Link className="btn btn-outline-light" to="/users/add">Add User</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
